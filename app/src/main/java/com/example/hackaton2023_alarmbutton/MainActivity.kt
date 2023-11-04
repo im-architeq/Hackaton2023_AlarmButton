9ebd05fbd0f03e9a5f180d04b1cf541a48f2d51c
ue88448 <ilja.mutschnik@sbb.ch>
2023-11-04 18:43:20 +0100

    initial state

---
+++ app/src/main/java/com/example/hackaton2023_alarmbutton/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.hackaton2023_alarmbutton
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.hackaton2023_alarmbutton.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonFirst.setOnClickListener { view ->
+            Snackbar.make(view, "Go to next screen", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
