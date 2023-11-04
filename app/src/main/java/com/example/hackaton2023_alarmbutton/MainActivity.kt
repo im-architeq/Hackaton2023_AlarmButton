203c26983c25676bcc7b4439bd886bbbade90141
ue88448 <ilja.mutschnik@sbb.ch>
2023-11-04 21:27:19 +0100

    add second activity

--- app/src/main/java/com/example/hackaton2023_alarmbutton/MainActivity.kt
+++ app/src/main/java/com/example/hackaton2023_alarmbutton/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.hackaton2023_alarmbutton
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.hackaton2023_alarmbutton.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,8 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonFirst.setOnClickListener { view ->
-            Snackbar.make(view, "Go to next screen", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val typeOfCommunicationActivity = Intent(this, TypeOfCommunicationActivity::class.java)
+            view.context.startActivity(typeOfCommunicationActivity)
+
         }
     }
 
---
+++ app/src/main/java/com/example/hackaton2023_alarmbutton/TypeOfCommunicationActivity.kt
@@ -0,0 +1,36 @@
+package com.example.hackaton2023_alarmbutton
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hackaton2023_alarmbutton.databinding.TypeOfCommunicationBinding
+
+class TypeOfCommunicationActivity : AppCompatActivity() {
+
+    private lateinit var binding: TypeOfCommunicationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = TypeOfCommunicationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonPhoneCall.setOnClickListener { view ->
+            Snackbar.make(view, "Make a phone call", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+
+        binding.buttonSignLanguage.setOnClickListener { view ->
+            Snackbar.make(view, "Make a signing session", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+
+        binding.buttonText.setOnClickListener { view ->
+            Snackbar.make(view, "Make a text message", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
